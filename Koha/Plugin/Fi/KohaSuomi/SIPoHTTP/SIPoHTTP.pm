32b7afbc27484e3f993fc4ec864c98685f7ddb22
janPasi <pasi.korkalo@ouka.fi>
2025-11-26 12:46:46 +0200

    Change configfile location

--- Koha/Plugin/Fi/KohaSuomi/SIPoHTTP/SIPoHTTP.pm
+++ Koha/Plugin/Fi/KohaSuomi/SIPoHTTP/SIPoHTTP.pm
@@ -315,7 +315,10 @@ sub extractServer {
     my ($term,       $pass) = getLogin($xmlmessage);
 
     #Handle all sip config XML files under /KOHA_CONF/SIPconfig
-    foreach my $file (glob("$CONFPATH/SIPconfig/*.xml")) {
+    #foreach my $file (glob("$CONFPATH/SIPconfig/*.xml")) {
+
+    # SIP configuration has been moved to a single file sipconfig.xml under KOHA_CONF -path
+    my $file = "$CONFPATH/sipconfig.xml";
 
         my $parser = XML::LibXML->new();
         my $doc    = XML::LibXML->load_xml(location => $file);
@@ -363,7 +366,7 @@ sub extractServer {
 
         }
 
-    }
+    # }
 
     $log->error("Missing SIPoHTTP account for $term in sip config XMLs");
     return 0;
